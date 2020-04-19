953e50833966d4b4acd353ae64d031a74e288b3e
alexandra <aleksandra_56@mail.ru>
2020-04-19 21:55:11 +0500

    скелет программы

---
+++ main_part.py
@@ -0,0 +1,18 @@
+class Shop:
+    def save_changes(file_name):
+        pass
+
+    def load_data(file_name, rewrite=False):
+        pass
+
+    def user_buy(user_name, shop_name, product_name, count, save_check=False):
+        pass
+
+    def find_product(product_name, count=-1):
+        pass
+
+    def sort_shops_by_product_price(product):
+        pass
+
+    def sort_shops_by_product_count(product):
+        pass
