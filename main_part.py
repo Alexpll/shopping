6edf507fb07a4509efd26acbcfd2e68c7a9f3c93
alexandra <aleksandra_56@mail.ru>
2020-05-07 20:11:50 +0500

    создан словарь данных

--- main_part.py
+++ main_part.py
@@ -16,3 +16,25 @@ class Shop:
 
     def sort_shops_by_product_count(product):
         pass
+
+
+dict_lines = {}
+text = open("shops.txt")
+count = 0
+for line in text:
+    new_list = []
+    lst = line.split(':')
+    lst[1] = lst[1].strip('\n').split(', ')
+    for i in lst[1]:
+        i = i.split(',')
+        new_i = []
+        for x in i:
+            if x.isdigit():
+                x = int(x)
+                new_i.append(x)
+            else:
+                new_i.append(x)
+        new_list.append(new_i)
+    dict_lines[lst[0]] = new_list
+text.close()
+print(dict_lines)
